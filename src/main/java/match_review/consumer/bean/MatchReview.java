981b060ae7af39208b637d43c2e8a584cfab25b5
pakitow <fcruzado@mail.stmarytx.edu>
2023-02-24 14:38:40 -0600

    Created DTO to map incoming event from REST_API into custom object: - Uses @JsonCreator - Matches <MatchReview> from rest_api.java

--- src/main/java/match_review/consumer/bean/MatchReview.java
+++ src/main/java/match_review/consumer/bean/MatchReview.java
@@ -2,13 +2,6 @@ package match_review.consumer.bean;
 
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
-
-//import lombok.AllArgsConstructor;
-//import lombok.Builder;
-//import lombok.Data;
-//import lombok.NoArgsConstructor;
-
-
 public class MatchReview {
     
     @JsonProperty("name") public String name;
