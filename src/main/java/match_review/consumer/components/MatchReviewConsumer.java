f70d2d275202940b10b2c810c64a8d886ce887bc
pakitow <fcruzado@mail.stmarytx.edu>
2023-02-25 11:40:50 -0600

    Update Consumer class - Linked to Service class - Process in batches

--- src/main/java/match_review/consumer/components/MatchReviewConsumer.java
+++ src/main/java/match_review/consumer/components/MatchReviewConsumer.java
@@ -1,35 +1,30 @@
 package match_review.consumer.components;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Component;
 
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import match_review.consumer.bean.MatchReview;
+import match_review.consumer.service.MatchReviewService;
 
 
 @RequiredArgsConstructor
 @Component
-@Slf4j
 public class MatchReviewConsumer {
     
     private final String topicName = "match_review";
+
+    @Autowired
+    private MatchReviewService matchReviewService;
     
     @KafkaListener(topics = topicName, groupId = "consumer_api", containerFactory = "matchReviewListenerContainer" )
     public void consume(
-        /* @Payload List<ConsumerRecord<String, MatchReview>> reviews */
         @Payload List<MatchReview> reviews
     ){
-        log.info("------------------------------");
-        log.info("Starting batch processing");
-        for(int i = 0; i < reviews.size(); i++){
-            log.info("Topic: {}", topicName);
-            log.info("MatchReview [name]: {}", reviews.get(i).name);
-            log.info("MatchReview [literal]: {}", reviews.get(i).literal);
-        }
-        log.info("batch was fully processed");
+        matchReviewService.processNewMatchReviews(reviews);
     }
     
 }
